6a38fb3e2249dcbdca2afd6a25f5b729183cc5a5
Jean <jeanmacario048@gmail.com>
2025-01-20 13:18:18 -0600

    BUILD\ Services page 20.02.2025-1318

--- resources/views/front/services/services-cards.blade.php
+++ resources/views/front/services/services-cards.blade.php
@@ -11,9 +11,9 @@
 
                 <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
 
-                    <div class="h-52 md:h-72 lg:h-72 min-h-5  relative mb-8 md:mb-20 lg:mb-8">
+                    <div class="border-4 border-red-500 h-52 md:h-72 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
                         <picture>
-                            <img class="w-full object-cover h-60 md:h-full" src="{{ asset('assets/img/rooms/simple.jpg') }}" alt="Habitaciones Hotel FCH Minerva">
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/rooms/simple.jpg') }}" alt="Habitaciones Hotel FCH Minerva">
                         </picture>
 
                         <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
@@ -21,7 +21,7 @@
                         </div>
                     </div>
 
-                    <div class="p-4 sm:p-6 flex flex-col  xl:h-80 text-center">
+                    <div class="border-4 border-red-500 p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
 
                         <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">18 Habitaciones</h2>
 
@@ -42,14 +42,48 @@
                 </div>
 
                 {{-- /card --}}
- 
+
                 {{-- card --}}
 
                 <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
 
-                    <div class="h-52 md:h-72 lg:h-72 min-h-5  relative mb-8 md:mb-20 lg:mb-8">
+                    <div class="h-52 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
                         <picture>
-                            <img class="w-full object-cover h-60 md:h-full" src="{{ asset('assets/img/services/servicio-habitacion.jpg') }}" alt="Habitaciones Hotel FCH Minerva">
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/services/desayuno.jpg') }}" alt="Restaurant Photography">
+                        </picture>
+
+                        <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
+                            <img class="service-icon w-14 text-center mx-auto" src="{{ asset('assets/icons/services/mug-hot.svg') }}" alt="Building">
+                        </div>
+                    </div>
+
+                    <div class="p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
+
+                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Desayuno</h2>
+
+                        <div>
+                            <p class="text-sm font-bold text-dark-medium leading-6">
+                                ¡Despierta con sabor! Proximante en nuestro restaurante 😊🍳
+                            </p>
+                        </div>
+                        
+                        <a class="hidden" target="_blank" href="https://direct-book.com/properties/HotelarboreaDirect">
+                            <button class="w-36 h-12 font-bold bg-ronda-orange text-white text-sm">Reservar Ahora</button>
+                        </a>
+
+                    </div>
+
+                </div>
+
+                {{-- /card --}}
+
+                {{-- card --}}
+
+                <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
+
+                    <div class="h-52 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
+                        <picture>
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/services/servicio-habitacion.jpg') }}" alt="Servicio a la habitación Hotel JA Centro">
                         </picture>
 
                         <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
@@ -57,9 +91,9 @@
                         </div>
                     </div>
 
-                    <div class="p-4 sm:p-6 flex flex-col  xl:h-80 text-center">
+                    <div class="p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
 
-                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Servicio a la habitación</h2>
+                        <h2 class="text-2xl font-bold my-4 sm:my-2">Servicio a la habitación</h2>
 
                         <div>
 
@@ -78,86 +112,14 @@
                 </div>
 
                 {{-- /card --}}
- 
-                {{-- card --}}
-
-                <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
-
-                    <div class="h-52 md:h-72 lg:h-72 min-h-5  relative mb-8 md:mb-20 lg:mb-8">
-                        <picture>
-                            <img class="w-full object-cover h-60 md:h-full" src="{{ asset('assets/img/services/desayuno.jpg') }}" alt="Habitaciones Hotel FCH Minerva">
-                        </picture>
-
-                        <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
-                            <img class="service-icon w-9 text-center mx-auto" src="{{ asset('assets/icons/services/mug-hot.svg') }}" alt="Building">
-                        </div>
-                    </div>
-
-                    <div class="p-4 sm:p-6 flex flex-col  xl:h-80 text-center">
-
-                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Desayuno</h2>
-
-                        <div>
-
-                            <p class="text-sm font-bold text-dark-medium leading-6">
-                                ¡Despierta con sabor! Proximante en nuestro restaurante 😊🍳
-                            </p>
-
-                        </div>
-                        
-                        <a class="hidden" target="_blank" href="https://direct-book.com/properties/HotelarboreaDirect">
-                            <button class="w-36 h-12 font-bold bg-ronda-orange text-white text-sm">Reservar Ahora</button>
-                        </a>
-
-                    </div>
-
-                </div>
-
-                {{-- /card --}}
- 
-                {{-- card --}}
-
-                <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
-
-                    <div class="h-52 md:h-72 lg:h-72 min-h-5  relative mb-8 md:mb-20 lg:mb-8">
-                        <picture>
-                            <img class="w-full object-cover h-60 md:h-full" src="{{ asset('assets/img/services/wifi.jpg') }}" alt="Habitaciones Hotel FCH Minerva">
-                        </picture>
-
-                        <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
-                            <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"></path></svg>
-                        </div>
-                    </div>
-
-                    <div class="p-4 sm:p-6 flex flex-col  xl:h-80 text-center">
-
-                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">WIFI Gratis</h2>
-
-                        <div>
-
-                            <p class="text-sm font-bold text-dark-medium leading-6">
-                                En todo el hotel
-                            </p>
-
-                        </div>
-                        
-                        <a class="hidden" target="_blank" href="https://direct-book.com/properties/HotelarboreaDirect">
-                            <button class="w-36 h-12 font-bold bg-ronda-orange text-white text-sm">Reservar Ahora</button>
-                        </a>
-
-                    </div>
-
-                </div>
-
-                {{-- /card --}}
                 
                 {{-- card --}}
 
                 <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
 
-                    <div class="h-52 lg:h-72 min-h-5 relative mb-8 md:mb-20 lg:mb-8">
+                    <div class="h-52 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
                         <picture>
-                            <img class="w-full object-cover md:h-full h-60" src="{{ asset('assets/img/services/television.jpg') }}" alt="Parking Photography">
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/services/television.jpg') }}" alt="Parking Photography">
                         </picture>
 
                         <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
@@ -165,7 +127,7 @@
                         </div>
                     </div>
 
-                    <div class="p-4 sm:p-6 flex flex-col xl:h-80 text-center">
+                    <div class="p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
 
                         <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Televisión</h2>
 
@@ -186,14 +148,51 @@
                 </div>
 
                 {{-- /card --}}
+                
+                {{-- card --}}
+
+                <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
+
+                    <div class="h-52 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
+                        <picture>
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/services/wifi.jpg') }}" alt="Wi-FI Hotel JA Centro">
+                        </picture>
+
+                        <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
+                            <svg fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M54.2 202.9C123.2 136.7 216.8 96 320 96s196.8 40.7 265.8 106.9c12.8 12.2 33 11.8 45.2-.9s11.8-33-.9-45.2C549.7 79.5 440.4 32 320 32S90.3 79.5 9.8 156.7C-2.9 169-3.3 189.2 8.9 202s32.5 13.2 45.2 .9zM320 256c56.8 0 108.6 21.1 148.2 56c13.3 11.7 33.5 10.4 45.2-2.8s10.4-33.5-2.8-45.2C459.8 219.2 393 192 320 192s-139.8 27.2-190.5 72c-13.3 11.7-14.5 31.9-2.8 45.2s31.9 14.5 45.2 2.8c39.5-34.9 91.3-56 148.2-56zm64 160a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"></path></svg>
+                            <svg hidden fill="#ffffff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M334.9 240.3c-8.703 1.531-14.52 9.812-12.97 18.53c9.609 54.5 19.27 135.3 10.73 211.1C332 476.2 327.6 480 323.9 480H264.9c-2.656 0-4.609-1.75-5.797-3.219c-2.75-3.438-3.766-8.281-2.734-13.03c9.078-41.56 24.06-123.9 22.62-208C278.9 246.9 271.4 240.7 262.7 240C253.9 240.2 246.9 247.4 247 256.3c1.391 80.91-13.11 160.4-21.88 200.6c-3.109 14.09 .2813 29 9.049 39.91C241.9 506.5 253.1 512 264.9 512h58.95c20.72 0 38.19-16.56 40.64-38.53c8.953-79.56-1.062-163.7-11.05-220.3C351.9 244.5 343.7 238.4 334.9 240.3zM409.6 58.19c-32.09 0-62.86 7.25-90.02 21.12C293.2 31.81 233.2 0 166.4 0C81.14 0 12.93 44.22 .543 107.5c-1.688 8.594 .6094 17.5 6.312 24.44C13.14 139.6 22.67 144 33.01 144h65.05C85.46 160.3 76.14 178.6 70.14 199c-19.36 65.72 3.656 129.1 11.58 140.3c5.141 7.219 13.25 11.81 22.27 12.62c1 .0938 2 .1562 3 .1562c8.674 0 17.13-3.438 23.69-9.781L266.5 208h276.5c10.34 0 19.88-4.375 26.16-12.03c5.703-6.938 8-15.84 6.312-24.41C562.8 106.9 491.5 58.19 409.6 58.19zM464 176v-32c0-8.844-7.158-16-16-16s-16 7.156-16 16v32H259.1c-4.203 0-8.25 1.656-11.25 4.625l-140.8 140.3c-3.609-5.875-24.36-61-5.75-117.3c13.77-41.66 45.99-72.88 95.74-92.78c8.203-3.281 12.2-12.59 8.922-20.78C204.4 83.81 198.3 80 191.1 80c-1.984 0-4 .375-5.938 1.156C164.8 89.66 146.2 100 130.3 112.2C129.6 112.1 128.8 112 127.1 112L31.95 113.7C41.21 66.38 97.76 32 166.4 32c61.27 0 115.1 30.94 130.8 75.28c1.641 4.594 5.297 8.188 9.922 9.781c4.625 1.562 9.719 .9375 13.81-1.719c25.58-16.44 56.24-25.16 88.63-25.16c66.89 0 124.7 37.63 133.4 85.81H464z"/></svg>
+                        </div>
+                    </div>
+
+                    <div class="p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
+
+                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">WIFI Gratis</h2>
+
+                        <div>
+
+                            <p class="text-sm font-bold text-dark-medium leading-6">
+                                En todo el hotel
+                            </p>
+
+                        </div>
+                        
+                        <a class="hidden" target="_blank" href="https://direct-book.com/properties/HotelarboreaDirect">
+                            <button class="w-36 h-12 font-bold bg-ronda-orange text-white text-sm">Reservar Ahora</button>
+                        </a>
+
+                    </div>
+
+                </div>
+
+                {{-- /card --}}
 
                 {{-- card --}}
 
                 <div class="flex flex-col lg:max-h-80 xl:h-80 max-w-[22.5rem] my-8 h-[32rem] min-h-[32rem] lg:min-h-[39rem] sm:basis-1/2 sm:max-w-1/2 lg:basis-1/3 lg:max-w-1/3 px-2">
 
-                    <div class="h-52 lg:h-72 min-h-5 relative mb-8 md:mb-20 lg:mb-8">
+                    <div class="h-52 lg:h-full min-h-5 basis-1/3 relative mb-8 md:mb-20 lg:mb-8">
                         <picture>
-                            <img class="w-full object-cover md:h-full h-60" src="{{ asset('assets/img/services/aire.jpg') }}" alt="Alberca Climatizada Hotel">
+                            <img class="w-full object-cover md:h-72 h-60" src="{{ asset('assets/img/services/aire.jpg') }}" alt="Aire acondicionado Hotel JA Centro">
                         </picture>
 
                         <div class="w-20 h-20 rounded-full bg-main p-4 absolute left-1/2 -translate-x-1/2 -bottom-16 md:-bottom-28 lg:-bottom-10">
@@ -201,9 +200,9 @@
                         </div>
                     </div>
 
-                    <div class="p-4 sm:p-6 flex flex-col xl:h-80 text-center">
+                    <div class="p-4 sm:p-6 flex flex-col basis-2/3 xl:h-80 text-center">
 
-                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Alberca climatizada</h2>
+                        <h2 class="text-2xl font-bold my-4 sm:my-2 capitalize">Aire aconidicionado</h2>
 
                         <div>
 
